1fbaff8ec7511b45ba93dc89f3a1d8ad1328f5e5
Siddhesh Shetye <siddhesh.2511shetye@gmail.com>
2025-07-03 00:06:38 +0530

    Routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express from "express";
+
 import productRoutes from "./routes/product.routes";
+import categoryRoutes from "./routes/category.routes";
+import orderRoutes from "./routes/order.routes";
+import userRoutes from "./routes/user.routes";
+import profileRoutes from "./routes/profile.routes";
 
 const app = express();
 
 app.use(express.json());
+
 app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/profiles", profileRoutes);
 
 export default app;
