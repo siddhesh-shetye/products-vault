4e99228467bcfde3fc560ad8422f1ff40df4d078
Siddhesh Shetye <siddhesh.2511shetye@gmail.com>
2025-07-09 23:54:57 +0530

    profile validation

--- src/validators/profile.ts
+++ src/validators/profile.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const profile = z.object({
-    firstName: z.string().min(1),
-    lastName: z.string().min(1),
-    phone: z.string().min(10),
-    address: z.string().min(1),
+    firstName: z.string().min(1, "First name is required"),
+    lastName: z.string().min(1, "Last name is required"),
+    phone: z.string().min(10, "Phone number must be at least 10 characters"),
+    address: z.string().min(1, "Address is required"),
 });
